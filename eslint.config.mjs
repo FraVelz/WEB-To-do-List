33712ab6a44b1875eb73ce3ebf3dd1a3ac71574e
Fravelz <fravelz_@hotmail.com>
2026-04-05 07:28:55 -0500

    feat: prettier+eslint aplyed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,11 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
-import tailwind from 'eslint-plugin-tailwindcss'
 import prettier from 'eslint-config-prettier'
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
-
-  {
-    plugins: {
-      tailwindcss: tailwind,
-    },
-    rules: {
-      ...tailwind.configs.recommended.rules,
-    },
-  },
 
   prettier, // SIEMPRE al final (desactiva conflictos)
 
